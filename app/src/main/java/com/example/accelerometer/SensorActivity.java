bcb1fc1f0725e2a6820368172d265d610a31feb0
1618042 <1618042.ky@sist.ac.jp>
2019-05-26 14:42:14 +0900

    1Hz,8Hz

--- app/src/main/java/com/example/accelerometer/SensorActivity.java
+++ app/src/main/java/com/example/accelerometer/SensorActivity.java
@@ -1,5 +1,6 @@
 package com.example.accelerometer;
 
+
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -18,7 +19,7 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+public class SensorActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
 
     Button start_button;
     Button stop_button;
@@ -30,44 +31,36 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     SimpleDateFormat simpleDateFormat;
     String nowDate;
     String time;
-    int Hz;
+    int Hz = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensor_main);
         manager = (SensorManager)getSystemService(Activity.SENSOR_SERVICE);
         sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        System.out.println(manager);
-        System.out.println(sensor);
-        start_button = findViewById(R.id.start_button);
-        start_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startclick();
-            }
-        });
-        stop_button = findViewById(R.id.stop_button);
-        stop_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopclick();
-            }
-        });
-        oneHz_button = findViewById(R.id.oneHz_button);
-        oneHz_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Hz = 1000;
-            }
-        });
-        eightHz_button = findViewById(R.id.eightHz_button);
-        eightHz_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Hz = 125;
-            }
-        });
+        buuttonset();
 
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Calendar calendar  = Calendar.getInstance();
+                simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SS", Locale.getDefault());
+                nowDate = simpleDateFormat.format(calendar.getTime());
+                time = nowDate;
+            }
+        },0,1000);
+
+    }
+    public void buuttonset(){
+        oneHz_button = findViewById(R.id.oneHz_button);
+        oneHz_button.setOnClickListener(this);
+        eightHz_button = findViewById(R.id.eightHz_button);
+        eightHz_button.setOnClickListener(this);
+        start_button = findViewById(R.id.start_button);
+        start_button.setOnClickListener(this);
+        stop_button = findViewById(R.id.stop_button);
+        stop_button.setOnClickListener(this);
     }
 
     public void onSensorChanged(SensorEvent event){ //センサーの値変更時の処理
@@ -77,35 +70,54 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
 
     public void onAccuracyChanged(Sensor sensor, int accuracy){ //センサー精度変更時の処理
     }
-    protected void onResume(){ super.onResume(); /*manager.registerListener(this, sensor, 625000);*/}
-    protected void onPause(){ super.onPause(); /*manager.unregisterListener(this);*/}
+    protected void onResume(){
+        super.onResume();
+        manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+
+    }
+    protected void onPause(){ super.onPause(); manager.unregisterListener(this);}
+    @Override
+    public void onClick(View view){
+        //System.out.println("onclick");
+        switch (view.getId()){
+            case R.id.oneHz_button :
+                Hz = 1000;
+                System.out.println("Hz : "+ Hz);
+                break;
+            case R.id.eightHz_button :
+                Hz = 125;
+                System.out.println("Hz : "+ Hz);
+                break;
+            case R.id.start_button :
+                startclick();
+                break;
+            case R.id.stop_button :
+                stopclick();
+                break;
+            default :
+                break;
+
+        }
+    }
     public void startclick(){
         manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Calendar calendar  = Calendar.getInstance();
-                simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SS", Locale.getDefault());
-                nowDate = simpleDateFormat.format(calendar.getTime());
-                time = nowDate;
-
-                if (event1!=null) {
-                    System.out.println(time+" : "+event1.values[0]);
+                if ( (event1 != null) || (Hz != 0) ) {
+                    System.out.println(time+" : event1 : "+event1.values[0]);
                     System.out.println(Hz);
                 }else {
-                    //System.out.println(time+" : null");
+                    System.out.println(time+" : event : null");
                 }
             }
         },0, Hz);//1Hz 1000ミリ秒, 8Hz 125ミリ秒
-
     }
     public void stopclick(){
         manager.unregisterListener(this);
         event1 = null;
-        if (event1==null) {
-            System.out.println(time+" : null");
-        }
+        System.out.println(time+" : null");
     }
 }
 
