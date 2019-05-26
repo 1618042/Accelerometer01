d31b5eac8a49a7bb8f39f50a3b2a4e6d8bb1f7cd
1618042 <1618042.ky@sist.ac.jp>
2019-05-26 17:10:01 +0900

    1Hz,8Hz

--- app/src/main/java/com/example/accelerometer/OpenHelper.java
+++ app/src/main/java/com/example/accelerometer/OpenHelper.java
@@ -1,4 +1,35 @@
 package com.example.accelerometer;
 
-public class OpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class OpenHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Test01.db";
+    private static final String TABLE_NAME = "Test01db";
+    private static final String _ID = "_id";
+    private static final String date = "date";
+    private static final String x_axis = "x_axis";
+    private static final String y_axis = "y_axis";
+    private static final String z_axis = "z_axis";
+    private static final String latitude = "latitude";
+    private static final String longitude = "longitude";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE "+ TABLE_NAME+ "("+ _ID + "INTEGER PRIMARY KEY,"
+                    +date+ "date,"+ x_axis+ "x_axis,"+y_axis+"y_axis,"
+                    +latitude+"latitude,"+longitude+"longitude)";
+    private static final String SQL_DERETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    OpenHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL(SQL_DERETE_ENTRIES);
+        onCreate(db);
+    }
 }
