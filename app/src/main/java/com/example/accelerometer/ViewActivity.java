a5be75b7130c3b849668bcba2be6b0576e1eaaba
1618042 <1618042.ky@sist.ac.jp>
2019-05-27 01:45:10 +0900

    view

--- app/src/main/java/com/example/accelerometer/ViewActivity.java
+++ app/src/main/java/com/example/accelerometer/ViewActivity.java
@@ -15,12 +15,15 @@ public class ViewActivity extends AppCompatActivity implements View.OnClickListe
     Cursor cursor;
     SQLiteDatabase db;
     ListView listView;
+    OpenHelper helper;
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
+        setContentView(R.layout.view_main);
 
         read_button = findViewById(R.id.read_button);
         read_button.setOnClickListener(this);
         listView = findViewById(R.id.view01);
+        listView.setAdapter(null);
 
     }
 
@@ -35,18 +38,21 @@ public class ViewActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void readData(){
+        helper = new OpenHelper(getApplicationContext());
+        db = helper.getReadableDatabase();
         try{
-            cursor = db.rawQuery("SELECT * from Test01db;",null);
+            cursor = db.rawQuery("SELECT * from Test01db",null);
             cursor.moveToFirst();
             if (cursor.getCount() > 0){
                 Integer[] data = new Integer[cursor.getCount()];
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item);
                 for (int cnt = 0; cnt < cursor.getCount(); cnt++){
                     data[cnt] = cursor.getInt(0);
-                    adapter.add("ID : "+cursor.getString(0)+", time : "+cursor.getString(1)+", x_axis :"+cursor.getString(2)+", y_axis : "+cursor.getString(3)+", z_axis : "+cursor.getString(4)+", latitude : "+cursor.getString(5)+", longitude : "+cursor.getString(6));
+                    adapter.add("ID : "+cursor.getString(0)+", time : "+cursor.getString(1)+", \nx_axis :"+cursor.getString(2)+", y_axis : "+cursor.getString(3)+", z_axis : "+cursor.getString(4)+", \nlatitude : "+cursor.getString(5)+", longitude : "+cursor.getString(6));
                     cursor.moveToNext();
                     listView.setAdapter(adapter);
                 }
+                cursor.close();
             }else {
                 listView.setAdapter(null);
             }
