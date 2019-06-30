c115ba712abb37c65805762c975cc3c0ed5e08c4
1618042 <1618042.ky@sist.ac.jp>
2019-06-30 10:18:44 +0900

    altitude, database

--- app/src/main/java/com/example/accelerometer/OpenHelper.java
+++ app/src/main/java/com/example/accelerometer/OpenHelper.java
@@ -5,7 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class OpenHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME = "Test01.db";
     private static final String TABLE_NAME = "Test01db";
     private static final String TABLE_NAME2 = "Management01db";
@@ -16,10 +16,12 @@ public class OpenHelper extends SQLiteOpenHelper {
     private static final String z_axis = "z_axis";
     private static final String latitude = "latitude";
     private static final String longitude = "longitude";
+    private static final String altitude = "altitude";
     private static final String filename = "filename";
-    private static final String SQL_CREATE_ENTRIES = " CREATE TABLE "+ TABLE_NAME + " ("+ _ID + " INTEGER PRIMARY KEY, " + time+ " varchar255,"+ filename+ " varchar255,"+ x_axis+ " Double, "+ y_axis+" Double, "+ z_axis +" Double," + latitude +" Double, "+ longitude +" Double )";
+    private static final String SQL_CREATE_ENTRIES = " CREATE TABLE "+ TABLE_NAME + " ("+ _ID + " INTEGER PRIMARY KEY, " + time+ " varchar255,"+ filename+ " varchar255,"+ x_axis+ " Double, "+ y_axis+" Double, "+ z_axis +" Double," + latitude +" Double, "+ longitude+ " Double, "+ altitude +" Double ) ";
     private static final String MSQL_CREATE_ENTRIES = " CREATE TABLE "+ TABLE_NAME2 + " ("+ _ID + " INTEGER PRIMARY KEY, " + filename+ " varchar255 )";
     private static final String SQL_DERETE_ENTRIES = " DROP TABLE IF EXISTS " + TABLE_NAME;
+    private static final String upgradeQuery = " ALTER TABLE Test01db ADD altitude Double ";
 
     OpenHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,9 +30,15 @@ public class OpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db){
         db.execSQL(MSQL_CREATE_ENTRIES);
         db.execSQL(SQL_CREATE_ENTRIES);
+        //System.out.println("ooooooooooooooooooooo");
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL(SQL_DERETE_ENTRIES);
-        onCreate(db);
+
+        //System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
+        /*if (newVersion > oldVersion) {
+            db.execSQL(upgradeQuery);
+        }*/
+        //db.execSQL(SQL_DERETE_ENTRIES);
+        //onCreate(db);
     }
 }
